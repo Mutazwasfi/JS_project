82e30052d38f89a42e9490e2dded46e14d744f6a
Mutazwasfi <Mutazwasfi5@gmail.com>
2023-07-24 09:47:56 +0300

    test

--- pages/analysisPage/analysisPage.js
+++ pages/analysisPage/analysisPage.js
@@ -2,6 +2,8 @@
 let showBtn = document.getElementById("resultBtn");
 let questionDiv = document.getElementById("Questions");
 
+// let showBtn = document.querySelector(".resultBtn");
+// let questionDiv = document.querySelector(".Questions")
 // Add a click event listener and functionality to the button  
 
 showBtn.addEventListener ('click', () => {
